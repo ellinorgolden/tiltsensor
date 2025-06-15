7705378224e5ff0cf3818c125dc7f45ddaaeb5aa
ellinorgolden <goldenellinor@gmail.com>
2025-06-15 12:12:54 +0200

    Parse input, change baurate

--- frontend/src/app/tilt.service.ts
+++ frontend/src/app/tilt.service.ts
@@ -3,13 +3,16 @@ import { io } from 'socket.io-client';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
+
 export class TiltService {
   private socket = io('http://localhost:3000');
 
-  getTiltStatus(): Observable<string> {
+  getTiltStatus(): Observable<boolean> { 
+
     return new Observable(observer => {
-      this.socket.on('tilt-status', (data: string) => {
-        observer.next(data);
+      this.socket.on('tilt-status', (data: any) => {
+        const isUp = data === true || data === '1' || data === 1;
+        observer.next(isUp);
       });
     });
   }
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
-#include <Arduino.h>
+#include <Arduino.h> 
 
-const int tiltPin = 8;  // Tilkoblet til 8 på Arduino
-
+const int tiltPin = 8; // Definerer hvilken digital pin tilt-sensoren er koblet til (her: D8)
+  
 void setup() {
-  pinMode(tiltPin, INPUT);     // Sett opp tiltPin som input
-  Serial.begin(9600);          // Start seriell kommunikasjon
+  pinMode(tiltPin, INPUT); 
+  Serial.begin(115200);  // 115200 baudrate 
 }
 
 void loop() {
-  int status = digitalRead(tiltPin);  // Les verdien fra sensoren
-
-  if (status == HIGH) {
-    Serial.println("Tilt-sensor: up");
-  } else {
-    Serial.println("Tilt-sensor: down");
-  }
-
-  delay(500);  // Vent litt for å unngå spam
+   Serial.println(digitalRead(tiltPin));
+  int status = digitalRead(tiltPin);  // tilt-sensoren: HIGH (1) = opp, LOW (0) = ned
+  Serial.println(status);           
+  delay(500); 
 }
